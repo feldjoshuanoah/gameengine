edc8d05e1f8234d0371d4e32d9f0a96df789df9c
feldjoshuanoah <feld.joshuanoah@gmail.com>
2022-11-13 16:13:31 +0100

    Create a texture abstraction

---
+++ src/main/java/com/feldjoshuanoah/gameengine/render/Texture.java
@@ -0,0 +1,71 @@
+/*
+ * Copyright 2022 Joshua Feld
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.feldjoshuanoah.gameengine.render;
+
+import org.lwjgl.BufferUtils;
+import org.lwjgl.opengl.GL11;
+import org.lwjgl.stb.STBImage;
+
+import java.nio.ByteBuffer;
+import java.nio.IntBuffer;
+
+/**
+ * Represents a texture.
+ */
+public class Texture {
+
+    /**
+     * The texture id.
+     */
+    private final int id;
+
+    /**
+     * Create a new texture.
+     *
+     * @param file The texture file.
+     */
+    public Texture(final String file) {
+        id = GL11.glGenTextures();
+        GL11.glBindTexture(GL11.GL_TEXTURE_2D, id);
+        GL11.glTexParameteri(GL11.GL_TEXTURE_2D, GL11.GL_TEXTURE_WRAP_S, GL11.GL_REPEAT);
+        GL11.glTexParameteri(GL11.GL_TEXTURE_2D, GL11.GL_TEXTURE_WRAP_T, GL11.GL_REPEAT);
+        GL11.glTexParameteri(GL11.GL_TEXTURE_2D, GL11.GL_TEXTURE_MIN_FILTER, GL11.GL_NEAREST);
+        GL11.glTexParameteri(GL11.GL_TEXTURE_2D, GL11.GL_TEXTURE_MAG_FILTER, GL11.GL_NEAREST);
+        final IntBuffer width = BufferUtils.createIntBuffer(1);
+        final IntBuffer height = BufferUtils.createIntBuffer(1);
+        final IntBuffer channels = BufferUtils.createIntBuffer(1);
+        final ByteBuffer image = STBImage.stbi_load(file, width, height, channels, 0);
+        if (image != null) {
+            GL11.glTexImage2D(GL11.GL_TEXTURE_2D, 0, GL11.GL_RGBA, width.get(0), height.get(0), 0,
+                    GL11.GL_RGBA, GL11.GL_UNSIGNED_INT, image);
+            STBImage.stbi_image_free(image);
+        }
+    }
+
+    /**
+     * Bind the texture.
+     */
+    public void bind() {
+        GL11.glBindTexture(GL11.GL_TEXTURE_2D, id);
+    }
+
+    /**
+     * Unbind the texture.
+     */
+    public void unbind() {
+        GL11.glBindTexture(GL11.GL_TEXTURE_2D, 0);
+    }
+}
